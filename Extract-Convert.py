967e2cc70343b80c6fa89ff0cbcb3289593306b8
aditiprakash <ap2205@gmail.com>
2017-12-16 11:56:03 +0530

    Update Extract-Convert.py

--- Extract-Convert.py
+++ Extract-Convert.py
@@ -8,5 +8,5 @@ pytrend = TrendReq(gmail_username, gmail_password, custom_useragent=None)
 #topic name specified with kw_list & timeframe specifies the time duartion to be considered
 pytrend.build_payload(kw_list=['pizza'], timeframe = 'today 12-m')
 interest_over_time_df = pytrend.interest_over_time()
-#name of the csv file to be stored is "pk.csv"
+#name of the csv file to be stored is "topic.csv"
 interest_over_time_df.to_csv("topic.csv", encoding='utf-8')
